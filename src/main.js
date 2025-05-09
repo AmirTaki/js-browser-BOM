282b067c80ad9fd339e5bfd638772f5dd87eb195
amir <amirtaki623@gmail.com>
2025-05-09 23:07:02 +0330

    reminder

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 document.getElementById("text").innerHTML = "folder Organiztion"
+
+let a = 2;
+let b = 7;
+let c = 12;
+
+console.log(a, b, c)
+/*
+تمام جاوااسکریپت (ساختمان داده آن) بر اساس ابجکت نوشته شده است
+*/
+let arr = new Array(1, 2, 3, 4, 5, -1, -2)
+
+/* property object*/
+
+console.log(arr.length)
+
+console.log(arr.pop())
+
+console.log(arr)
+
+/*
+هرآبحکت توسط یک کلاس به وجود می آید
+هر آبجکت کلاس مربوط به خود را دارد
+وهر ابجکت یک سری پراپرتی دارد که نقش داده داردو 
+هر متد که نقش اکشن دارد
+*/
