0d47fdc3c8d18066a30f78515dfa013b9daa130f
amir <amirtaki623@gmail.com>
2025-05-10 00:54:20 +0330

    property 'history' BOM

--- src/main.js
+++ src/main.js
@@ -1,45 +1,15 @@
-document.getElementById("text").innerHTML = " property 'loaction' BOM "
+document.getElementById("text").innerHTML = " property 'history' BOM "
+
+
+window.history
+
+window.history.length  // تغداد تاریخچه تب
+
+window.history.back() // برگشت 
+
+window.history.forward()  // جلو
+
+window.history.go(-1) // یک صفحه به عفب
 
 
 
-// url : Protocol , ip (domain) , Port , Path
-/*
-http://127.0.0.1:5500/index.html
-Protocol : لایه اپلیکیشن شبکه => http
-ip : addres server =? 127.0.0.1
-port : 5500
-path :index.html  نمایش کدام صفخه در وب
-
-*/
-
-window.location
-
-window.location.hostname // ip or domain name
-
-window.location.protocol // protocol
-
-window.location.port   // port
-
-window.location.pathname   // name web page 
-
-window.location.hash  // bookmark html ->   #about اسامی قسمتی از صفحات که در انجاییم
-// link http://127.0.0.1:5500/index.html#about 
-
-window.location.search  // search in url نمایش
-
-window.location.href  // url full
-
-window.location.host    // id/admin + port
-
-window.location.origin // protocol + id/admin + port
-
-
-
-
-window.location.assign("")  // go to page 
-
-window.location = "";
-
-window.location.reload // refresh page
-
-window.replace("") // go to page not back 
