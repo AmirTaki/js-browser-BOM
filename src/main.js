22f28c3faac52b3c3b8abdc5b25edf3609960b9c
amir <amirtaki623@gmail.com>
2025-05-10 14:03:40 +0330

    Toturil BOM : javaScript in Browsers

--- src/main.js
+++ src/main.js
@@ -1,14 +1,3 @@
-document.getElementById("text").innerHTML = " gio location : Geographical location : BOM"
+document.getElementById("tet").innerHTML = " "
 
 
-// Geographical Location => GPS
-
-window.navigator.geolocation.getCurrentPosition()
-
-window.navigator.geolocation.getCurrentPosition((p)=>{
- console.log(`${p.coords.latitude}, ${p.coords.longitude}`)
-})
-
-
-
-// service =>  mapbox.com => create account
