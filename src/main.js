8d8d1b9cdee1d31285dd7cbcaeb2e4dbcb7ccfee
amir <amirtaki623@gmail.com>
2025-05-10 13:00:03 +0330

    Storage in Browser

--- src/main.js
+++ src/main.js
@@ -1,30 +1,7 @@
-document.getElementById("text").innerHTML = " property 'screen' BOM "
+document.getElementById("text").innerHTML = " Storage in browser  BOM "
 
-//  screen : size
+// Local Storage
 
-window.innerHeight;     // ارتفاع تب مرورگر
+// Sesstion Storate
 
-window.innerWidth;      // عرق تب مرورگر
-
-window.outerHeight      // ارتفاع مرورگر
-
-window.outerWidth        // عرض مرورگر
-
-// sccreen : سایز صفحه نمایش کاریر
-
-window.screen;
-
-window.screen.width;        // عرض صفحه نمایش
-
-window.screen.height;       // ارتفاغ صفحه نمایش
-
-window.screen.availWidth    //  در اختیار داشتن عرض صفحه نمایش
-
-window.screen.availHeight    //  در اختیار داشتن ارتفاع صفحه نمایش
-
-window.screen.orientation.type // عمودی یا افقی بودن صفحه نمایش کاربر
-
-// 'landscape-primary'  : صفحه افقی ->  labtop
-// 'portrait-primary'   : صفحه عمودی -> mobile
-// 'portrait-secondary' : صفحه عمودی برعکس-> mobile
-//  'landscape-secondary' : صفحه افقی -> mobile 
+// Cookie
