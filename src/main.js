893da2da5c198bcedcad5aad54d3ea0c5953dc88
amir <amirtaki623@gmail.com>
2025-05-10 13:19:31 +0330

    Cookie  : storage in browser : BOM

--- src/main.js
+++ src/main.js
@@ -1,7 +1,21 @@
-document.getElementById("text").innerHTML = " Storage in browser  BOM "
+document.getElementById("text").innerHTML = " Cookie  : storage in browser : BOM "
 
-// Local Storage
 
-// Sesstion Storate
 
 // Cookie
+
+window.document
+
+window.document.cookie
+
+window.document.cookie = "Product_name = ball;"  // ذخیره کوکی 
+
+window.document.cookie = "name = amir; max-age = 5" // ذخیره کوکی با تاریخ انقضا
+
+new Date("2025/1/1").toUTCString()  
+
+// / ذخیره کوکی با تاریخ انقضا
+window.document.cookie = "name = mani ; expires = Tue, 31 Dec 2024 20:30:00 GMT"
+
+// حذف کوکی با تاریخی در گذشته
+window.document.cookie = "name = mani ; expires = Tue, 31 Dec 1990 20:30:00 GMT"
